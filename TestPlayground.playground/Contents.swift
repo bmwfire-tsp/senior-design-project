68e1565585df3b3bc89df650fde768058d1c5f6e
Brandon Wong <bwong7@hawaii.edu>
2019-03-21 18:25:01 -1000

    Completed Edge Permutations

---
+++ TestPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+func edgeComb(n: Int, k: Int) -> [[String]] {
+    var edges = [[Int]]()
+    var result = [[String]]()
+    
+    func combinations(offset: Int, partial: [Int]) {
+        if partial.count == k {
+            edges.append(partial)
+            return
+        }
+        
+        let numRemaining = k - partial.count
+        var i = offset
+        while i <= n && numRemaining <= n - i + 1 {
+            combinations(offset: i + 1, partial: partial + [i])
+            i += 1
+        }
+    }
+    
+    combinations(offset: 1, partial: [Int]())
+    for edge in edges {
+        result.append([locations[edge[0] - 1], locations[edge[1] - 1]])
+        result.append([locations[edge[1] - 1], locations[edge[0] - 1]])
+    }
+    return result
+}
+let locations = ["Aiea", "Manoa", "Kalihi", "Ewa", "Honolulu"]
+let edges = edgeComb(n: locations.count, k: 2)
+print(locations)
+print(edges)
